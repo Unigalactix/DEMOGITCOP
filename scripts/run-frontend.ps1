8ef70ea85abe58ab115162fddfcc964f20e0c9bf
Rajesh Kodaganti (Rugved) <kodagantir295@gmail.com>
2025-12-08 14:16:23 -0800

    Cleanup: remove old files, keep fastapi-calculator only

--- scripts/run-frontend.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = 'Stop'
-Push-Location "$PSScriptRoot\..\frontend"
-try {
-  if (-not (Test-Path node_modules)) { npm install }
-  npm run dev
-} finally {
-  Pop-Location
-}
